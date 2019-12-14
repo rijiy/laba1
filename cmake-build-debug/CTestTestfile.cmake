36995f802e029af7640a2f162b5c9685dd5217e0
rijiy <smolyaninovaa@student.bmstu.ru>
2019-12-14 09:22:42 +0300

    kjfk4

--- cmake-build-debug/CTestTestfile.cmake
+++ cmake-build-debug/CTestTestfile.cmake
@@ -1,8 +1,8 @@
 # CMake generated Testfile for 
-# Source directory: /cygdrive/c/Git/lab-01-parser
-# Build directory: /cygdrive/c/Git/lab-01-parser/cmake-build-debug
+# Source directory: /cygdrive/c/laba1/laba1
+# Build directory: /cygdrive/c/laba1/laba1/cmake-build-debug
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
-add_test(unit_tests "/cygdrive/c/Git/lab-01-parser/cmake-build-debug/tests.exe")
-set_tests_properties(unit_tests PROPERTIES  _BACKTRACE_TRIPLES "/cygdrive/c/Git/lab-01-parser/CMakeLists.txt;58;add_test;/cygdrive/c/Git/lab-01-parser/CMakeLists.txt;0;")
+add_test(unit_tests "/cygdrive/c/laba1/laba1/cmake-build-debug/tests.exe")
+set_tests_properties(unit_tests PROPERTIES  _BACKTRACE_TRIPLES "/cygdrive/c/laba1/laba1/CMakeLists.txt;58;add_test;/cygdrive/c/laba1/laba1/CMakeLists.txt;0;")
--- cmake-build-debug/_3rdParty/Hunter/toolchain/_builds/cmake_install.cmake
+++ cmake-build-debug/_3rdParty/Hunter/toolchain/_builds/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /cygdrive/c/laba1/laba1
+# Install script for directory: /cygdrive/c/laba1/laba1/cmake-build-debug/_3rdParty/Hunter/toolchain
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -12,7 +12,7 @@ if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
     string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
            CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
   else()
-    set(CMAKE_INSTALL_CONFIG_NAME "Debug")
+    set(CMAKE_INSTALL_CONFIG_NAME "")
   endif()
   message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
 endif()
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/cygdrive/c/laba1/laba1/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/cygdrive/c/laba1/laba1/cmake-build-debug/_3rdParty/Hunter/toolchain/_builds/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
